2e426f415f0e8192cd49dc5ef21835282b38feb8
QwetYMaN <121778817+QwetYMaN@users.noreply.github.com>
2023-01-02 11:49:01 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,4 @@ import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('omega 3 & Blueberry Oatmeal')
-srteamlit.text('kale, spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+
