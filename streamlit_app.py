b1c6ca52bdc1b785624fb34715c5e25638904884
QwetYMaN <121778817+QwetYMaN@users.noreply.github.com>
2023-01-02 12:11:25 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,7 @@ streamlit.text('Hard-Boiled Free-Range Egg')
 
 import pandas 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #Lets put a pick list here so they can pick the fruit they want to include
 streamlit.multiselect("pick some fruits:", list(my_fruit_list.index))
