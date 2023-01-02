6ab063017b52a6054426c6605a999e49c4a6a015
QwetYMaN <121778817+QwetYMaN@users.noreply.github.com>
2023-01-02 11:52:01 -0800

    Update streamlit_app.py
    Typo changed

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
 
 streamlit.text('omega 3 & Blueberry Oatmeal')
-srteamlit.text('kale, spinach & Rocket Smoothie')
+streamlit.text('kale, spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
